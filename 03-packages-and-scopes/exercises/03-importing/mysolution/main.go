b7893ed3937f8bd67d0158078f48286014d71d61
Sri Harsh Amur <sriharshamur@gmail.com>
2022-09-27 15:33:32 +0200

    03-packages-and-scopes Completed

---
+++ 03-packages-and-scopes/exercises/03-importing/mysolution/main.go
@@ -0,0 +1,34 @@
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
+
+package main
+
+// ---------------------------------------------------------
+// EXERCISE: Rename imports
+//
+//  1- Import fmt package three times with different names
+//
+//  2- Print a few messages using those imports
+//
+// EXPECTED OUTPUT
+//  hello
+//  hey
+//  hi
+// ---------------------------------------------------------
+
+import (
+	fmt "fmt"
+	output "fmt"
+	print "fmt"
+)
+
+func main() {
+	fmt.Println("Printing from fmt")
+	print.Println("Printing from print")
+	output.Println("Printing from outputt")
+}
